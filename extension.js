f595aac632f4ef0700c99e06017f50c00f1c8813
eexpress <eexpress@163.com>
2022-01-22 21:52:29 +0800

    5

--- extension.js
+++ extension.js
@@ -1,18 +1,4 @@
 /* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
@@ -30,28 +16,26 @@ const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 
+const logprefix = "===clip-note===>";
+
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
     _init() {
         super._init(0.0, _('Clip Note'));
 
-        this.add_child(new St.Icon({
-            icon_name: 'face-smile-symbolic',
-            style_class: 'system-status-icon',
-        }));
+        this.add_child(new St.Icon({ gicon: Gio.icon_new_for_string(Me.path+"/clip-note-symbolic.svg") }));
         //~ ---------------------------------------------
-        log("------------\tClip Note\t---------------");
-        const logpath = Me.path+"/../../../clip-note";
+        const logpath = GLib.get_home_dir()+"/.local/share/clip-note";
+        log(logprefix+logpath);
         //~ UserDirectory.DIRECTORY_DOWNLOAD
-        log(Me.path);
         let f4array;
         if(!GLib.file_test(logpath,GLib.FileTest.IS_DIR)){
 			GLib.mkdir_with_parents(logpath, 0o700);
 			f4array = ["1.web", "2.live", "3.tech", "4.other"];
 		}
-        log("------------\tClip Note\t---------------");
-		const r = GLib.file_get_contents(logpath+"/test");
-		log(r[1]);	GLib.free(r[1]);
+        //~ log("------------\tClip Note\t---------------");
+		//~ const r = GLib.file_get_contents(logpath+"/test");
+		//~ log(r[1]);	GLib.free(r[1]);
 
 
         //~ ---------------------------------------------
@@ -60,7 +44,7 @@ class Indicator extends PanelMenu.Button {
 		const butt = [];
 		//~ vbox.add_child(butt);
 		["edit-copy-symbolic", "document-new-symbolic", "tools-check-spelling-symbolic", "edit-delete-symbolic"].forEach((str, i)=>{
-			const icon = new St.Icon({ icon_name: str, style_class: "cn-iconlist" });
+			const icon = new St.Icon({ icon_name: str, icon_size: 32, style_class: "cn-icon" });
 			butt[i] = new St.Button({ can_focus: true, child: icon, hover: true});
 			//~ butt.actor.toggle-mode = true;
 			//~ butt.actor.checked = true;
@@ -94,22 +78,26 @@ class Indicator extends PanelMenu.Button {
 			let info;
 			while ((info = fileEnum.next_file(null))){
 				const fname = info.get_name();
-				log(`--->\t${fname}`);
-				const item = new PopupMenu.PopupMenuItem('<span foreground="blue" size="32">fname</span>');
-				item.actor.set_size(300,80);
-				//~ item.actor.create_pango_layout('<span foreground="blue" size="32">fname</span>');
-				//~ item.actor.first_child.set_markup('<span foreground="blue" size="32">fname</span>');
-				//~ set_markup
-				//~ item.actor.paint();
-				item.filename = fname;	//addition
-				item.style_class = 'large_text';
-				item.can_focus = true;
+				log(logprefix+fname);
+				const item = new PopupMenu.PopupMenuItem(fname, {style_class:'ct-text', can_focus:true});
+				item.label.clutter_text.set_markup(split2pango(fname));
+				item.filename = fname;	//additional properties
 				item.connect('activate', (actor) => {
 					log(`${actor.filename} click.`);
 				});
 				this.menu.addMenuItem(item);
 			}
 		}
+        //~ ---------------------------------------------
+        function split2pango(str){
+			const color = ['#00193E','#6196E6','#42CC53','#E68061','#E6617A','#D361E6'];
+			const seg = str.split('.');
+			let out = '';
+			seg.forEach((s, i)=>{
+				out += '  <span background="'+color[(s.length-1)%color.length]+'"><b> '+s+' </b></span>';
+			});
+			return out;
+		};
         //~ ---------------------------------------------
     }
 });
