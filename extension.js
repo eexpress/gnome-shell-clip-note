4568728ff321a3192b3ac07f219683b2af8a831e
eexpress <eexpress@163.com>
2022-01-23 19:25:11 +0800

    refresh列表

--- extension.js
+++ extension.js
@@ -41,13 +41,24 @@ class Indicator extends PanelMenu.Button {
         const mact = new PopupMenu.PopupBaseMenuItem({reactive: false});
 		const hbox = new St.BoxLayout();
 		const butt = [];
-		["edit-copy-symbolic", "document-new-symbolic", "document-open-symbolic","tools-check-spelling-symbolic", "edit-delete-symbolic", "view-refresh-symbolic"].forEach((str, i)=>{
+		//~ ["edit-copy-symbolic", "document-new-symbolic", "document-open-symbolic","tools-check-spelling-symbolic", "edit-delete-symbolic", "view-refresh-symbolic"].forEach((str, i)=>{
+		["edit-copy-symbolic", "view-refresh-symbolic"].forEach((str, i)=>{
 			const icon = new St.Icon({ icon_name: str, icon_size: 32, style_class: "cn-icon" });
 			butt[i] = new St.Button({ can_focus: true, child: icon, toggle_mode: true });
+			butt[i].name = str;
 			//~ checked 状态由 css `:checked` 控制。瞎猜出来的。
 			butt[i].connect('clicked', (self) => {
 				butt.forEach((self)=>{ self.checked = false; });
 				self.checked = true;
+				log(logprefix+self.name);
+				if(self.name == "view-refresh-symbolic"){
+					this.menu._getMenuItems().forEach((j)=>{	//_getMenuItems()看源码找出来的。
+						if(j.filename) j.destroy();	//PopupMenuItem
+					});
+					refresh_menu(this, ls(savepath));
+					self.checked = false;
+					butt[0].set_checked(true);
+				}
 			});
 			hbox.add_child(butt[i]);
 		});
@@ -58,29 +69,33 @@ class Indicator extends PanelMenu.Button {
         //~ ---------------------------------------------
         this._clipboard = St.Clipboard.get_default();
         //~ Read files from savepath, Creat PopupMenuItem.
-        ls(savepath).forEach((fname)=>{
-			const item = new PopupMenu.PopupMenuItem(fname, {style_class:'ct-text', can_focus:true});
-			item.label.clutter_text.set_markup(split2pango(fname));
-			item.filename = fname;	//additional properties
-			item.connect('activate', (actor) => {
-				//~ log(`${actor.filename} click.`);
-				this._clipboard.get_text(St.ClipboardType.PRIMARY, (clipboard, text) => {
-					if(text){
-						const f = savepath+"/"+actor.filename;
-			//~ const r = GLib.ByteArray.toString(GLib.file_get_contents(f)[1]); //Error Content
-//~ Some code called array.toString() on a Uint8Array instance. Previously this would have interpreted the bytes of the array as a string, but that is nonstandard. In the future this will return the bytes as comma-separated digits. For the time being, the old behavior has been preserved, but please fix your code anyway to explicitly call ByteArray.toString(array).
-//~ (Note that array.toString() may have been called implicitly.)
-//~ 0 _init/</</<() ["/home/eexpss/.local/share/gnome-shell/extensions/clip-note@eexpss.gmail.com/extension.js":76:16]
-						const r = GLib.file_get_contents(f)[1];
-						const c = new Date();
-						const t = r+"\n------  "+c.getFullYear()+"-"+c.getMonth()+1+"-"+c.getDate()+" "+c.getHours()+":"+c.getMinutes()+":"+c.getSeconds()+"  ------\n"+text+"\n";
-						GLib.file_set_contents(f,t);
-					}
-				});
-			});
-			this.menu.addMenuItem(item);
-		});
+		refresh_menu(this, ls(savepath));
         //~ ---------------------------------------------
+		function refresh_menu(owner, list){
+			list.forEach((fname)=>{
+				const item = new PopupMenu.PopupMenuItem(fname, {style_class:'ct-text', can_focus:true});
+				item.label.clutter_text.set_markup(split2pango(fname));
+				item.filename = fname;	//additional properties
+				item.connect('activate', (actor) => {
+					log(logprefix+actor.filename+"click.");
+					owner._clipboard.get_text(St.ClipboardType.PRIMARY, (clipboard, text) => {
+						if(text){
+							const f = savepath+"/"+actor.filename;
+				//~ const r = GLib.ByteArray.toString(GLib.file_get_contents(f)[1]); //Error Content
+	//~ Some code called array.toString() on a Uint8Array instance. Previously this would have interpreted the bytes of the array as a string, but that is nonstandard. In the future this will return the bytes as comma-separated digits. For the time being, the old behavior has been preserved, but please fix your code anyway to explicitly call ByteArray.toString(array).
+	//~ (Note that array.toString() may have been called implicitly.)
+	//~ 0 _init/</</<() ["/home/eexpss/.local/share/gnome-shell/extensions/clip-note@eexpss.gmail.com/extension.js":76:16]
+							const r = GLib.file_get_contents(f)[1];
+							const c = new Date();
+							const t = r+"\n------  "+c.getFullYear()+"-"+c.getMonth()+1+"-"+c.getDate()+" "+c.getHours()+":"+c.getMinutes()+":"+c.getSeconds()+"  ------\n"+text+"\n";
+							GLib.file_set_contents(f,t);
+						}
+					});
+				});
+				owner.menu.addMenuItem(item);
+			});
+		};
+         //~ ---------------------------------------------
         function ls(path){	//return an array of files
 			const dir = Gio.File.new_for_path(path);
 			let fileEnum;
