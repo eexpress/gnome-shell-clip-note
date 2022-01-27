e49871c5e6a54834177b413460ce8c290bd748bc
eexpress <eexpress@163.com>
2022-01-27 10:20:04 +0800

    fix tabs

--- extension.js
+++ extension.js
@@ -19,37 +19,37 @@ const logprefix = "===clip-note===>";
 
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
-    _init() {
-        super._init(0.0, _('Clip Note'));
+	_init() {
+		super._init(0.0, _('Clip Note'));
 
-        this.add_child(new St.Icon({ gicon: Gio.icon_new_for_string(Me.path+"/clip-note-symbolic.svg") }));
-        //~ =============================================
-        //~ Creat New Directory and New Files
-        //~ ---------------------------------------------
-        const savepath = GLib.get_home_dir()+"/.local/share/clip-note";
-        const init_file_array = ["1.web.javascript", "2.live.skill", "3.tech.clip", "4.other"];
-        if(!GLib.file_test(savepath,GLib.FileTest.IS_DIR)){
+		this.add_child(new St.Icon({ gicon: Gio.icon_new_for_string(Me.path+"/clip-note-symbolic.svg") }));
+		//~ =============================================
+		//~ Creat New Directory and New Files
+		//~ ---------------------------------------------
+		const savepath = GLib.get_home_dir()+"/.local/share/clip-note";
+		const init_file_array = ["1.web.javascript", "2.live.skill", "3.tech.clip", "4.other"];
+		if(!GLib.file_test(savepath,GLib.FileTest.IS_DIR)){
 			GLib.mkdir_with_parents(savepath, 0o700);
 			init_file_array.forEach((i)=>{
 				GLib.file_set_contents(savepath+"/"+i,'auto create file.\n');
 			});
 		}
-        //~ =============================================
-        //~ Creat Icon list
-        //~ ---------------------------------------------
+		//~ =============================================
+		//~ Creat Icon list
+		//~ ---------------------------------------------
 		//~ "document-open-symbolic","tools-check-spelling-symbolic", "edit-delete-symbolic"
-        const gname = ["copy","open","new","refresh"];
-        const gicon = ["edit-copy-symbolic", "folder-open-symbolic", "document-new-symbolic", "view-refresh-symbolic"];
-        const gtoggle = [true, false, true, false];
-        const gtip = [_("Copy Clip to file below"), _("Open Notes directory"), _("Add a new file"), _("Refresh file list")];
+		const gname = ["copy","open","new","refresh"];
+		const gicon = ["edit-copy-symbolic", "folder-open-symbolic", "document-new-symbolic", "view-refresh-symbolic"];
+		const gtoggle = [true, false, true, false];
+		const gtip = [_("Copy Clip to file below"), _("Open Notes directory"), _("Add a new file"), _("Refresh file list")];
 
-        const mact = new PopupMenu.PopupBaseMenuItem({reactive: false});
+		const mact = new PopupMenu.PopupBaseMenuItem({reactive: false});
 		const hbox = new St.BoxLayout();
 		const butt = [];
 		gname.forEach((str, i)=>{
 			const icon = new St.Icon({ icon_name: gicon[i], icon_size: 30, style_class: "cn-icon", track_hover: true });
 			butt[i] = new St.Button({ child: icon, toggle_mode: gtoggle[i], style_class:'cn' });
-			butt[i].name = str;	//additional properties
+			butt[i].name = str; //additional properties
 			//~ checked 状态由 css `:checked` 控制。瞎猜出来的。
 			butt[i].connect('style-changed', (self) => {
 				hover_text(self, gtip[i]);
@@ -64,7 +64,7 @@ class Indicator extends PanelMenu.Button {
 						GLib.spawn_command_line_async('xdg-open '+savepath);
 						break;
 					case "refresh":
-						this.menu._getMenuItems().forEach((j)=>{	//_getMenuItems()看源码找出来的。删除全部文件的 PopupMenuItem
+						this.menu._getMenuItems().forEach((j)=>{    //_getMenuItems()看源码找出来的。删除全部文件的 PopupMenuItem
 							if(j.filename) j.destroy();
 						});
 						refresh_menu(this, ls(savepath));
@@ -77,7 +77,7 @@ class Indicator extends PanelMenu.Button {
 		butt[0].set_checked(true);
 		this.menu.addMenuItem(mact);
 
-        function virtual_click(self){
+		function virtual_click(self){
 			const isAddNewFile = (self === butt[2]) ? true : false;
 			input.set_reactive(isAddNewFile);
 			butt.forEach((self)=>{ self.checked = false; });
@@ -87,12 +87,13 @@ class Indicator extends PanelMenu.Button {
 			if(isAddNewFile){
 				input.text = "";
 				input.hint_text = _("Input filename here.");
+				input.clutter_text.grab_key_focus();
 			}
 		};
 
 		const minput = new PopupMenu.PopupBaseMenuItem({reactive: false});
 		const input = new St.Entry({ name: 'input', style_class: 'cn-text', x_expand: true, can_focus: true, reactive: false});
-//~ clutter_input_focus_set_cursor_location: assertion 'clutter_input_focus_is_focused (focus)' failed	第一次显示tip时（写入时）出现。
+//~ clutter_input_focus_set_cursor_location: assertion 'clutter_input_focus_is_focused (focus)' failed  第一次显示tip时（写入时）出现。
 		input.clutter_text.connect('activate', (actor) => {
 			if(butt[2].checked){
 				add_menu(this, input.text);
@@ -102,24 +103,24 @@ class Indicator extends PanelMenu.Button {
 		});
 		minput.add(input);
 		this.menu.addMenuItem(minput);
-        //~ =============================================
-        //~ Read files from savepath, Creat PopupMenuItem.
+		//~ =============================================
+		//~ Read files from savepath, Creat PopupMenuItem.
 		//~ ---------------------------------------------
-        this._clipboard = St.Clipboard.get_default();
+		this._clipboard = St.Clipboard.get_default();
 		refresh_menu(this, ls(savepath));
 
 		function refresh_menu(owner, list){
 			list.forEach((fname)=>{ add_menu(owner,fname); });
 		};
 
-        function hover_text(self, str){
+		function hover_text(self, str){
 			if(!input.get_reactive()){
 				if(self.hover) input.text = str;
 				else input.text = "";
 			}
 		};
 
-        function add_menu(owner,fname){
+		function add_menu(owner,fname){
 			const item = new PopupMenu.PopupBaseMenuItem({style_class:'cn-text'});
 			const icon0 = new St.Icon({ icon_name: "document-open-symbolic", icon_size: 24 });
 			const icon1 = new St.Icon({ icon_name: "view-app-grid-symbolic", icon_size: 24 });
@@ -141,8 +142,8 @@ class Indicator extends PanelMenu.Button {
 				if(self.hover) self.child = icon0; else self.child = icon1;
 			});
 
-			butt.filename = fname;	//additional properties
-			item.filename = fname;	//additional properties
+			butt.filename = fname;  //additional properties
+			item.filename = fname;  //additional properties
 			item.connect('activate', (actor) => {
 				owner._clipboard.get_text(St.ClipboardType.PRIMARY, (clipboard, text) => {
 					if(text){
@@ -160,7 +161,7 @@ class Indicator extends PanelMenu.Button {
 			owner.menu.addMenuItem(item);
 		 };
 
-        function ls(path){	//return an array of files
+		function ls(path){  //return an array of files
 			const dir = Gio.File.new_for_path(path);
 			let fileEnum;
 			let r = [];
@@ -174,7 +175,7 @@ class Indicator extends PanelMenu.Button {
 			return r;
 		};
 
-        function split2pango(str){
+		function split2pango(str){
 			const color = ['#00193E','#6196E6','#42CC53','#E68061','#E6617A','#D361E6'];
 			const seg = str.split('.');
 			let out = '';
@@ -183,27 +184,27 @@ class Indicator extends PanelMenu.Button {
 			});
 			return out;
 		};
-    }
+	}
 });
 
 class Extension {
-    constructor(uuid) {
-        this._uuid = uuid;
+	constructor(uuid) {
+		this._uuid = uuid;
 
-        ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
-    }
+		ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
+	}
 
-    enable() {
-        this._indicator = new Indicator();
-        Main.panel.addToStatusArea(this._uuid, this._indicator);
-    }
+	enable() {
+		this._indicator = new Indicator();
+		Main.panel.addToStatusArea(this._uuid, this._indicator);
+	}
 
-    disable() {
-        this._indicator.destroy();
-        this._indicator = null;
-    }
+	disable() {
+		this._indicator.destroy();
+		this._indicator = null;
+	}
 }
 
 function init(meta) {
-    return new Extension(meta.uuid);
+	return new Extension(meta.uuid);
 }
